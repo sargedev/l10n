6d442fffe97692d4816bfcde865eef9c41b152ca
Sarge <63020240+sargedev@users.noreply.github.com>
2026-02-26 18:22:56 +0100

    Added block representation to basic functions

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
 
+/**
+ * Provides library for localizing text in user projects
+ */
+//% color="#d10247" weight=0 icon="\uf1ab" block="Localization"
 namespace l10n {
 
     interface SymbolTable {
@@ -29,27 +33,39 @@ namespace l10n {
 
     let LOCALE: Locale = null;
 
+    //% blockId=l10n_setLocale
+    //% block="set language to $locale"
     export function setLocale(locale: Locale) {
         LOCALE = locale;
     }
 
+    //% blockId=l10n_removeLocale
+    //% block="unset current language"
     export function removeLocale() {
         LOCALE = null;
     }
 
+    //% blockId=l10n_getLocale
+    //% block="get current language"
     export function getLocale() {
         return LOCALE;
     }
 
+    //% blockId=l10n_isLocaleSet
+    //% block="is language set"
     export function isLocaleSet(): boolean {
         return !!LOCALE;
     }
 
+    //% blockId=l10n_t
+    //% block="localized $name || with args $args"
     export function t(name: string, args?: string[]) {
         if (!LOCALE) throw "Language locale is undefined";
         return tl(LOCALE, name, args);
     }
 
+    //% blockId=l10n_tl
+    //% block="localized $name in language $locale || with args $args"
     export function tl(locale: Locale, name: string, args?: string[]) {
         let result = locale.get(name);
         if (!result) throw `Identifier '${name}' not found in locale '${locale.code}'`;
